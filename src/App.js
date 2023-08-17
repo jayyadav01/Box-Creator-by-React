927c3d485f895e28d9b8ad867b11540bc157d62a
jayyadav01 <jayyadav.rbt@gmail.com>
2023-08-18 03:22:03 +0530

    Box Creator

--- src/App.js
+++ src/App.js
@@ -1,19 +1,58 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 import './App.css'
-import { useState } from 'react'
 
 function App() {
 
-    const[Iswidth,setwidth] = useState();
-    const[Isheight,setheight] = useState();
-    const[Iscolor,setcolor] = useState('');
+    const inputRef1 = useRef(null);
+    const inputRef2 = useRef(null);
+    const inputRef3 = useRef(null);
+
+    const[Iswidth,setIswidth] = useState();
+    const[Isheight,setIsheight] = useState();
+    const[Iscolor,setIscolor] = useState('');
     
     function handleBox()
     {
         let box = document.querySelector('#bg');
+
         box.style.backgroundColor = `${Iscolor}`;
         box.style.width = `${Iswidth}px`;
         box.style.height = `${Isheight}px`;
+        
+        setIswidth('');
+        setIsheight('');
+        setIscolor('');
+
+        inputRef1.current.focus();
+    }
+    
+    function key1down(event)
+    {
+      if(event.key === 'Enter')
+      {
+        event.preventDefault();
+        inputRef2.current.focus();
+      }
+
+    }
+
+    function key2down(event)
+    {
+      if(event.key === 'Enter')
+      {
+        event.preventDefault();
+        inputRef3.current.focus();
+      }
+    }
+
+    function key3down(event)
+    {
+        let button = document.querySelector('button');
+        if(event.key === 'Enter')
+        {
+            event.preventDefault();
+            button.click();
+        }
     }
 
   return (
@@ -21,18 +60,19 @@ function App() {
         <div className='parent'>
           <label>
             Width :  
-            <input type='number' value={Iswidth} onChange={(e) => setwidth(e.target.value)} /> <br/>
+            <input ref={inputRef1} onKeyDown={key1down} type='text' value={Iswidth} onChange={(e) => setIswidth(e.target.value)} /> <br/>
           </label>
           <label>       
             Height :
-            <input type='number' value={Isheight} onChange={(e) => setheight(e.target.value)} /> <br/>
+            <input ref={inputRef2} onKeyDown={key2down} type='text' value={Isheight} onChange={(e) => setIsheight(e.target.value)} /> <br/>
           </label> 
           <label>
             Background Color :
-            <input type='text' value={Iscolor} onChange={(e) => setcolor(e.target.value)} /> <br/>
+            <input ref={inputRef3} onKeyDown={key3down} type='text' value={Iscolor} onChange={(e) => setIscolor(e.target.value)} /> <br/>
           </label>
           
           <button onClick={handleBox} >Check</button>
+
           <div id='bg' ></div>
 
         </div>
